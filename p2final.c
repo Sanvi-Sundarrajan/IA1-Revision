4b1e0871c5bfeca74ee728142002be7d9f83a5ce
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-14 06:19:17 +0000

    p2

--- p2final.c
+++ p2final.c
@@ -3,3 +3,41 @@ int input()
 int cmp(int a, int b, int c)
 void output(int a, int b, int c, int largest)*/
 #include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter a number \n");
+  scanf("%d",&n);
+  return n;
+  
+}
+int cmp(int a, int b, int c)
+{
+  if(a>b&&a>c)
+  {
+    return a;
+
+  }
+  else if (b>c)
+  {
+    return b;
+  }
+  else
+  {
+    return c;
+  }
+}
+void output(int a, int b, int c, int largest)
+{
+  printf("The largest of %d , %d and %d is %d",a,b,c,largest);
+}
+int main()
+{
+int a,b,c,largest;
+  a=input();
+  b=input();
+  c=input();
+  largest = cmp(a,b,c);
+  output(a,b,c,largest);
+  return 0;
+}
