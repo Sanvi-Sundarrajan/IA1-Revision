1ee1edcf37a6b081f30f3c91e835d2a4e2b752a1
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-14 06:03:58 +0000

    p1 debug

---
+++ p2final.c
@@ -0,0 +1,5 @@
+/* Write a program to find largest of 3 numbers using 4 functions using Pass by value
+int input()
+int cmp(int a, int b, int c)
+void output(int a, int b, int c, int largest)*/
+#include<stdio.h>
